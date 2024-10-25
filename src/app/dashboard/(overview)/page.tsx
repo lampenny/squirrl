5c7f14be2e000f550292f69084df1266862ae192
Penny L <pennylam@live.com>
2024-10-25 21:17:21 +0900

    Add last updated

--- src/app/dashboard/(overview)/page.tsx
+++ src/app/dashboard/(overview)/page.tsx
@@ -1,19 +1,27 @@
-import { Suspense } from 'react';
-import { format } from 'date-fns';
-import CardWrapper from "@/app/ui/dashboard/cardwrapper";
-import { CardSkeleton } from '@/app/ui/skeletons';
+import { Suspense } from "react";
+import { format } from "date-fns";
+import CardWrapper from "@/app/ui/dashboard/card-wrapper";
+import { CardSkeleton, ChartSkeleton } from "@/app/ui/skeletons";
+import SavingsCharts from "@/app/ui/dashboard/savings-charts";
+import HeatMap from "@/app/ui/dashboard/heatmap";
 
 export default async function Page() {
   const now = new Date();
   const date = format(now, "EEEE do MMMM");
+  const shortDate = format(now, "dd/MM/yyyy");
 
   return (
     <div>
-      <h1 className="text-2xl pb-4">{date}</h1>
-      <div className="grid gap-6 grid-cols-2 lg:grid-cols-3">
-        <Suspense fallback={<CardSkeleton />}>
-          <CardWrapper />
-        </Suspense>
+      <div className="flex flex-row space-x-8 pb-4 place-items-baseline">
+        <h1 className="text-2xl">{date}</h1>
+        <p className="text-sm text-gray-400">last updated: {shortDate}</p>
+      </div>
+      <div className="w-full flex flex-row">
+        <div className="grid gap-6 grid-cols-2">
+          <Suspense fallback={<CardSkeleton />}>
+            <CardWrapper />
+          </Suspense>
+        </div>
       </div>
     </div>
   );
